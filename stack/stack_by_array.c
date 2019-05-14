4fe2e23d5415be5cae682440eebd7629da0d2070
Jimmy Ma <a468963@gmail.com>
2019-05-14 23:47:13 +0800

    add stask

---
+++ stack/stack_by_array.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#define MAX 10
+
+void push(void);
+void pop(void);
+void list(void);
+void peek(void);
+void isEmpty(void);
+
+// 用來追蹤堆疊位置
+int top = -1;
+
+// 存放string 使用二維陣列
+char item[MAX][20];
+
+int main(){
+
+}
+//push 如果堆疊沒有滿就把值放到stask中
+void push(void){
+
+}
+
+//pop 移除一項 如果堆疊沒有資料存在 需要顯示錯誤
+void pop(void){
+
+}
+
+// 顯示所有值
+void list(void){
+
+}
+
+// 取出最上面的值
+void peek(void){
+
+}
+
+// 確認是否為空
+void isEmpty(void){
+
+}
