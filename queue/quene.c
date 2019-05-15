0c6d86e7fb57ccff1cf19d74690e0c8be32b6f8f
Jimmy Ma <a468963@gmail.com>
2019-05-16 00:22:58 +0800

    add note

---
+++ queue/quene.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MAX 10
+
+// 此程式沒有考慮到刪除的資料空間無法作用 導致空間會用完的問題
+
+void enqueue(void);
+void dequeue(void);
+
+// 目標陣列 
+char a[MAX][10];
+
+int main(){
+
+}
+
+// 當rear >= MAX-1表示達到最大容量 否則rear+1且存入a
+void enqueue(void){
+
+}
+
+// 當rear = front 表示沒有元素 否則front增加即可刪除a的第一個元素
+void dequeue(void){
+
+}
