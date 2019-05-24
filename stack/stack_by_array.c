320d853916fb4dc227ceac433a6e0ae337c6d0ee
jimmyma <jimmyma@croxlink.com>
2019-05-24 11:42:33 +0800

    修改部分stack

--- stack/stack_by_array.c
+++ stack/stack_by_array.c
@@ -2,44 +2,42 @@
 // * pop ()，Pop: Removes an item from the stack. The items are popped in the reversed order in which they are pushed. If the stack is empty, then it is said to be an Underflow condition.
 // * peek()，peekPeek or Top: Returns top element of stack.
 // * isEmpty()，isEmpty: Returns true if stack is empty, else false
+// 目前只有數字才會成功，輸入字元會有無窮迴圈bug
 #include <stdio.h>
 #include <stdlib.h>
 
 #define ARRAY_MAX 10
 #define EMPTY -1
 
-int isEmpty();
-void peek();
-void push(int);
-void pop();
+int isEmpty(void);
+void peek(void);
+void push(void);
+void pop(void);
+void list(void);
 
+// 之後可用二維陣列存str, char item[MAX][20];
 int stack[ARRAY_MAX];
 int top = EMPTY;
+int choice;
 
 int main()
 {
-	while (1)
+	do
 	{
-		int choice, data;
 		printf("\n");
 		printf("-----Choose the function you want :-----\n");
 		printf("1. push data\n");
 		printf("2. pop data\n");
 		printf("3. peek data\n");
-		printf("4. check isEmpty\n");
+		printf("4. list data\n");
 		printf("5. Exit\n");
 		printf("----------------------------------------\n");
 		printf("your choice: ");
 		scanf("%d", &choice);
-
 		switch (choice)
 		{
 		case 1:
-			printf("\n");
-			printf("Input data:");
-			scanf("%d", &data);
-			printf("test %d\n", data);
-			push(data);
+			push();
 			break;
 		case 2:
 			pop();
@@ -48,41 +46,34 @@ int main()
 			peek();
 			break;
 		case 4:
-			if (isEmpty())
-			{
-				printf("it's empty!");
-			}
-			else
-			{
-				printf("it's not empty!");
-			}
+			list();
 			break;
 		case 5:
-			// exit(0)會結束main程式，相當於return 0
-			exit(0);
+			printf("\nEXIT POINT ");
+			break;
 		default:
 			printf("Error!\n");
 			break;
 		}
 		printf("\n");
-	}
+	} while (choice != 5);
 	return 0;
 }
 
+// 確認是否為空
 int isEmpty()
 {
 	if (top == EMPTY)
 	{
-		// printf("stask is empty\n");
 		return 1;
 	}
 	else
 	{
-		// printf("stask is not empty\n");
 		return 0;
 	}
 }
 
+// 取出最上面的值
 void peek()
 {
 	if (isEmpty())
@@ -95,73 +86,45 @@ void peek()
 	}
 }
 
-void push(int c)
+//push 如果堆疊沒有滿就把值放到stask中
+void push()
 {
-	if (top == ARRAY_MAX)
+	int data;
+	if (top >= ARRAY_MAX - 1)
 	{
 		printf("stask is full\n");
 	}
 	else
 	{
+		printf("\nInput data(only can input int now):");
+		scanf("%d", &data);
 		top++;
-		stack[top] = c;
-		printf("push data %d to stask\n\n", c);
+		stack[top] = data;
 	}
 }
 
+//pop 移除一項 如果堆疊沒有資料存在 需要顯示錯誤
 void pop()
 {
-	if (top == EMPTY)
+	if (isEmpty())
 	{
 		printf("stask is empty now !\n");
 	}
 	else
-	{	
+	{
 		printf("pop data %d from stask\n\n", stack[top]);
 		top--;
 	}
 }
-/*
-
-#include <stdio.h>
-#define MAX 10
-
-void push(void);
-void pop(void);
-void list(void);
-void peek(void);
-void isEmpty(void);
-
-// 用來追蹤堆疊位置
-int top = -1;
-
-// 存放string 使用二維陣列
-char item[MAX][20];
-
-int main(){
-
-}
-//push 如果堆疊沒有滿就把值放到stask中
-void push(void){
-
-}
-
-//pop 移除一項 如果堆疊沒有資料存在 需要顯示錯誤
-void pop(void){
-
-}
 
 // 顯示所有值
-void list(void){
-
+void list(){
+	int i;
+	if(isEmpty()){
+		printf("stask is empty now !\n");
+	}else{
+		for(i = 0; i<top+1; i++){
+			printf("stask %d is %d\n", i, stack[i]);
+		}
+	}
 }
-
-// 取出最上面的值
-void peek(void){
-
-}
-
-// 確認是否為空
-void isEmpty(void){
-
-*/
