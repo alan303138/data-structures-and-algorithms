e7b589038d1718a589a2d6d6f56db77dc0b23898
jimmyma <jimmyma@croxlink.com>
2019-06-24 12:07:10 +0800

    add note

--- linked_list/demo_6_24.c
+++ linked_list/demo_6_24.c
@@ -4,12 +4,28 @@
 struct node
 {
 	int number;
-	struct node *next;	
+	struct node *next;
 };
 typedef struct node Node;
 
 Node *head = NULL;
 
+// 不知道如何操作struct，待完成 todo
+void print_all_node()
+{
+	if (head == NULL)
+	{
+		printf("this list is empty");
+	}
+
+	Node *temp_node = head;
+
+	while (temp_node)
+	{
+		printf("temp_node ");
+		temp_node = temp_node->next;
+	}
+}
 
 int main()
 {
@@ -17,16 +33,21 @@ int main()
 	{
 		int choice;
 		printf("\n--Choose one choice--\n");
-		printf("1. prinf hello world\n");
-		printf("2. exit\n");
+		printf("1. prinf all data\n");
+		printf("2. add data\n");
+		printf("3. exit\n");
 		printf("your choice:\n");
 		scanf("%d", &choice);
 
 		if (choice == 1)
 		{
+			print_all_node();
+		}
+		else if (choice == 2)
+		{
 			printf("hello world\n");
 		}
-		else if(choice == 2)
+		else if (choice == 3)
 		{
 			exit(0);
 		}
