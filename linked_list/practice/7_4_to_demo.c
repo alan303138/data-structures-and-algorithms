962f98c80c20149771935da6dd6ab9ffbe03c34e
jimmyma <jimmyma@croxlink.com>
2019-07-08 11:25:22 +0800

    add note

--- linked_list/practice/7_4_to_demo.c
+++ linked_list/practice/7_4_to_demo.c
@@ -19,7 +19,6 @@ int main()
 	Node *first, *node;
 
 	Node a, b, c;
-
 	first = &a;
 
 	a.data = 12;
@@ -56,25 +55,28 @@ void printList(Node *first)
 Node *addNewNode(Node *first, int data)
 {
 	Node *current, *new_node;
-	new_node = (Node *)malloc(sizeof(Node));
 	if (first == NULL)
 	{	
-		first = (Node *)malloc(sizeof(Node)); 
-		current->data = data;
-		current->next = NULL;
+		// 先建立一個new node再把資料放到first
+		new_node = (Node *)malloc(sizeof(Node)); 
+		new_node->data = data;
+		new_node->next = NULL;
+		first = new_node; 
 	}
 	else
 	{	
 		// 找出最後一項，並帶入值
 		current = first;
-		while (current != NULL)
+		while (current->next != NULL)
 		{
 			current = current->next;
 		}
+		// 建立一個新的Node
 		new_node = (Node *)malloc(sizeof(Node));
-		current = new_node;
-		current->data = data;
-		current->next = NULL;	
+		new_node->data = data;
+		new_node->next = NULL;
+		// 把新的Node放到最後一項的下一個，成為新的最後一項
+		current->next = new_node;	
 	}
 
 	return first;
