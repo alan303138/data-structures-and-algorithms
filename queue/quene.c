25ab3c8ce504e2da74596588708754a8a5ffac45
jimmyma <jimmyma@croxlink.com>
2019-08-12 18:30:21 +0800

    add note

--- queue/quene.c
+++ queue/quene.c
@@ -10,16 +10,35 @@ void dequeue(void);
 // 目標陣列 
 char a[MAX][10];
 
-int main(){
+// front表前端，資料離開的地方，rear代表尾端，資料加入的地方
+int front = 0, rear = 0;
 
+int main(){
+	enqueue();
+	enqueue();
+	dequeue();
+	dequeue();
+	dequeue();
 }
 
 // 當rear >= MAX-1表示達到最大容量 否則rear+1且存入a
 void enqueue(void){
-
+	if(rear >= MAX-1){
+		printf("The Queue is full \n");
+	}else {
+		rear++;
+		printf("please enter an item to queue: ");
+		// todo 處理此行編譯會警告的問題
+		scanf("%s", &a[rear]);
+	}
 }
 
 // 當rear = front 表示沒有元素 否則front增加即可刪除a的第一個元素
 void dequeue(void){
-
+	if(front == rear){
+		printf("The queue is empty \n");
+	}else {
+		front++;
+		printf("delete %s from quere \n", a[front]);
+	}
 }
